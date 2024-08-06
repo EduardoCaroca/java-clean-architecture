559877ca730844413f81d52e2e4a64854be71c1f
eduardo.caroca <eduardo.figueiredo@seasolutions.io>
2024-08-06 19:02:00 -0300

    refactor(CRUD): Remove Controller, Model, Repository and Service

--- src/main/java/clean/architecture/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package clean.architecture.repository;
-
-import clean.architecture.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
